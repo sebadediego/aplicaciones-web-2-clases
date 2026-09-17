d69e22e8415161547ec8fd4584dd6ba856e4f9da
sebadediego <152932725+sebadediego@users.noreply.github.com>
2026-09-16 22:38:27 -0300

    Completar clase 10

---
+++ clase10/index-middlewares.mjs
@@ -0,0 +1,31 @@
+import express from 'express'
+
+const PUERTO = 3000
+
+const app = express()
+app.listen(PUERTO)
+
+
+// MIDDLEWARES
+
+const mid1 = (req, res, next) => {
+    console.log('se ejecuto el middleware 1')
+    next()
+}
+const mid2 = (req, res, next) => {
+    console.log('se ejecuto el middleware 2')
+    next()
+}
+
+// app.use('/', mid1)
+app.use('/saludo', mid1)
+
+
+app.get('/', (req, res) => {
+    console.log('/')
+    res.end()
+})
+app.get('/saludo', (req, res) => {
+    console.log('/saludo')
+    res.end()
+})
